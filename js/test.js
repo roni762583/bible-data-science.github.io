b5414f6c0bc7fc1a517037d210b776fd1a0becb1
Aharon Zbaida <roni762583@gmail.com>
2019-03-10 19:18:59 -0400

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,13 +1,16 @@
-document.getElementById("test").innerHTML = 5 + 6;
-console.log("hello from test.js");
+//document.getElementById("test").innerHTML = 5 + 6;
+
 if (localStorage && !localStorage.getItem('size')) {
+  console.log("testing local storage from test.js");
     var i = 0;
     try {
         // Test up to 10 MB
         for (i = 250; i <= 10000; i += 250) {
             localStorage.setItem('test', new Array((i * 1024) + 1).join('a'));
+            console.log("reached "+str(i)+" MB of local storage");
         }
     } catch (e) {
+        console.log("exception: "+String(e));
         localStorage.removeItem('test');
         localStorage.setItem('size', i - 250);
     }
